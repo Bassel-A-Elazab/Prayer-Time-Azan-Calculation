6f664774864e42dce947492944c661143c5d034d
bassel ahmed alazab <bassel.alazab@gmail.com>
2020-07-12 06:08:38 +0200

    Getter and Setter of all input variable to can access it

--- main.cpp
+++ main.cpp
@@ -11,15 +11,64 @@ private:
     tm *currentDate = localtime(&getTime);
 
 public:
-    PrayerTime(double newLong,double newLat,double newTimeZone,double newH,double,double newGD,double newGN,int newSH){
+    PrayerTime(double newLong,double newLat,double newTimeZone,double newHeight,double,double newGD,double newGN,int newSH){
         Long = newLong;
         Lat = newLat;
         timeZone = newTimeZone;
-        H = newH;
+        Height = newHeight;
         GD = newGD;
         GN = newGN;
         SH = newSH;
         dayOfYear = currentDate->tm_yday;
+    }
+    //Set & Get Longitude Of Your Country
+    void setLong(double newLong){
+        Long = newLong;
+    }
+    double getLong(){
+        return Long;
+    }
+    //Set & Get latitude Of Your Country
+    void setLat(double newLat){
+        Lat = newLat;
+    }
+    double getLat(){
+        return Lat;
+    }
+    //Set & Get Time Zone Of Your Country
+    void setTimeZone(double newTimeZone){
+        timeZone = newTimeZone;
+    }
+    double getTimeZone(){
+        return timeZone;
+    }
+    //Set & Get Height Above See Of Your Country
+    void setHeight(double newHeight){
+        Height = newHeight;
+    }
+    double getHeight(){
+        return Height;
+    }
+    //Set & Get Dawn’s Angle Twilight Of Your Country
+    void setGD(double newGD){
+        GD = newGD;
+    }
+    double getGD(){
+        return GD;
+    }
+    //Set & Get Night’s Twilight Angle Of Your Country
+    void setGN(double newGN){
+        GN = newGN;
+    }
+    double getGN(){
+        return GN;
+    }
+    //Set And Get The Choice Of Your Mazhab.
+    void setSH(double newSH){
+        SH = newSH;
+    }
+    double getSH(){
+        return SH;
     }
 
 };
