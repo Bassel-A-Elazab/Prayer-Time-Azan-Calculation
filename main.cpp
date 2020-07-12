405684b22b2a6ce298ff4cf2bcd9fb7587313148
bassel ahmed alazab <bassel.alazab@gmail.com>
2020-07-12 05:59:06 +0200

    Create class that is calculate all equation to get prayer time

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 #include <ctime>
 using namespace std;
 
+class PrayerTime{
+private:
+    double Long,Lat,timeZone,H,GD,GN;
+    int SH,dayOfYear;
+    time_t getTime = time(0);
+    tm *currentDate = localtime(&getTime);
 
+public:
+    PrayerTime(double newLong,double newLat,double newTimeZone,double newH,double,double newGD,double newGN,int newSH){
+        Long = newLong;
+        Lat = newLat;
+        timeZone = newTimeZone;
+        H = newH;
+        GD = newGD;
+        GN = newGN;
+        SH = newSH;
+        dayOfYear = currentDate->tm_yday;
+    }
+
+};
 int main()
 {
     cout << "Hello world!" << endl;
